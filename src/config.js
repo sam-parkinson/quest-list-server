11badba404da0023e1c2c92c71d40ddd59b06714
Sam Parkinson <parkinsonsp42@gmail.com>
2019-08-05 12:23:40 -0400

    Prepare for Heroku db deploy

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-  DB_URL: process.env.DB_URL,
+  DB_URL: process.env.DATABASE_URL || process.env.DB_URL,
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 }
